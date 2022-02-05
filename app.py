42257856b0484b4b5381d42f0e0abbc271352d4c
akhilkathi97 <akhilkathi.1997@gmail.com>
2022-02-05 20:46:00 +0530

    Response Modified

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,json
 import gunicorn
 
 app = Flask(__name__)
@@ -20,16 +20,18 @@ def ocr_api(documentName):
        }
     }"""
     try:
-        response = textract.analyze_expense(
+        res = textract.analyze_expense(
            Document={
                 "S3Object": {
                     "Bucket": "invoice-ocr-poc.22",
                     "Name": documentName
                 }
             })
+        response = app.response_class(response=json.dumps(res),status=200,mimetype='application/json')
         return response
         #pretty_printed_string = get_string(textract_json=response, output_type=[Textract_Expense_Pretty_Print.SUMMARY, Textract_Expense_Pretty_Print.LINEITEMGROUPS], table_format=Pretty_Print_Table_Format.fancy_grid)
         #print(pretty_printed_string)
     except Exception as e_raise:
         print(e_raise)
-        return e_raise
+        return app.response_class(response=e_raise,status=400)
+
