18940c88c7a13823dffa53b187224402d754c298
akhilkathi97 <akhilkathi.1997@gmail.com>
2022-02-05 21:22:51 +0530

    Added imports

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template,json,jsonify
 import gunicorn
+import boto3
+import os
+from textractprettyprinter.t_pretty_print_expense import get_string, Textract_Expense_Pretty_Print, Pretty_Print_Table_Format
+from flask import Flask, jsonify, request,send_file
 
 app = Flask(__name__)
 
