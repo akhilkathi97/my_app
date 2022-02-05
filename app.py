b1cb1b66adb63d11eb13e4b90ab56d9b82e532aa
akhilkathi97 <akhilkathi.1997@gmail.com>
2022-02-05 20:29:54 +0530

    OCR Testing

--- app.py
+++ app.py
@@ -7,3 +7,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ocrApi/<documentName>')
+def ocr_api(documentName):
+    """{
+       "Document": { 
+          "Bytes": blob,
+          "S3Object": { 
+             "Bucket": "string",
+             "Name": "string",
+             "Version": "string"
+          }
+       }
+    }"""
+    try:
+        response = textract.analyze_expense(
+           Document={
+                "S3Object": {
+                    "Bucket": "invoice-ocr-poc.22",
+                    "Name": documentName
+                }
+            })
+        return response
+        #pretty_printed_string = get_string(textract_json=response, output_type=[Textract_Expense_Pretty_Print.SUMMARY, Textract_Expense_Pretty_Print.LINEITEMGROUPS], table_format=Pretty_Print_Table_Format.fancy_grid)
+        #print(pretty_printed_string)
+    except Exception as e_raise:
+        print(e_raise)
+        return e_raise
